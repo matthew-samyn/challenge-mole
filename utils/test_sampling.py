ac2129b291c0302bc4c5c106c27bb99c91c9ca5f
matthew-samyn <84380381+matthew-samyn@users.noreply.github.com>
2021-09-22 16:33:01 +0200

    Less broken file

--- utils/test_sampling.py
+++ utils/test_sampling.py
@@ -11,26 +11,12 @@ import shutil
 #understanding the folders size
 dictionary_size_folders = {'akiec': 262, 'bcc': 411, 'bkl': 879, 'df': 92, 'mel': 890, 'nv': 5364, 'vasc': 114}
 
-
 #creating the folders to copy the images
 
 current_dir = os.getcwd()
-
-os.chdir("../train")
-os.mkdir('../copes')
-folder_to_copy = "../copies"
-
-# Create test and validate folders, containing label folders
-for main_folder in folders_to_create:
-    os.mkdir(main_folder)
-    os.chdir(main_folder)
-    for label_folder in list(labels.keys()):
-        os.mkdir(label_folder)
-    os.chdir("..")
-
-
-
 os.chdir("../data/train")
+os.mkdir("../copy_directory")
+copy_directory = "../../copy_directory"
 
 #destination_folder = []
 #for dir in os.getcwd():
@@ -49,25 +35,22 @@ for folder in os.listdir():
         print('before resampling: ', len(images))
         for c in os.listdir():
             for x in range(10):
-                old_file = os.path.join(os.getcwd(), c)
-                new_file = os.path.join(os.getcwd(), f"{c}_{x}")
-                os.rename(old_file, new_file)
+                shutil.copy(c, f"{copy_directory}")
+                os.rename(f"{copy_directory}/{c}", f"{c[:-4]}_{x}.jpg")
         print('before after resampling: ', len(images))
     elif 150 < len(images) < 600:
         print('before resampling: ', len(images))
         for c in os.listdir():
             for x in range(4):
-                old_file = os.path.join(os.getcwd(), c)
-                new_file = os.path.join(os.getcwd(), f"{c}_{x}")
-                os.rename(old_file, new_file)
+                shutil.copy(c, copy_directory)
+                os.rename(f"{copy_directory}/{c}", f"{c[:-4]}_{x}.jpg")
         print('before after resampling: ', len(images))
     elif 600 < len(images) < 1200:
         print('before resampling: ', len(images))
         for c in os.listdir():
             for x in range(2):
-                old_file = os.path.join(os.getcwd(), c)
-                new_file = os.path.join(os.getcwd(), f"{c}_{x}")
-                os.rename(old_file, new_file)
+                shutil.copy(c, f"{copy_directory}")
+                os.rename(f"{copy_directory}/{c}", f"{c[:-4]}_{x}.jpg")
     os.chdir(current_directory)
 
 current_dir = os.getcwd()
