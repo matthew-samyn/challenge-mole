1fedb04b15138cfc8968bfad34007d42d2595610
Leonor Drummond <84380184+ltadrummond@users.noreply.github.com>
2021-09-22 11:05:00 +0200

    add test_sampling

--- utils/test_sampling.py
+++ utils/test_sampling.py
@@ -11,34 +11,34 @@ import shutil
 os.chdir("../data/train")
 
 #destination_folder = []
-for dir in os.getcwd():
-    longest_dir_length = max(len(dir))
-    for folder in os.listdir():
-        current_directory = os.getcwd()
-        os.chdir(folder)
-        images = os.listdir()
-        if len(images) > 5000:
-            print(os.getcwd())
-            print('before resampling: ', len(images))
-            for c in random.sample(os.listdir(), (len(images) / 2)):
-                shutil.move(c, os.chdir('../../trash_images'))
-            print('before after resampling: ', len(images))
-        if len(images) < 150:
-            print(os.getcwd())
-            print('before resampling: ', len(images))
-            for c in random.sample(os.listdir(), test_amount):
-                shutil.copy(c, os.getcwd())
-            print('before after resampling: ', len(images))
-        elif 300 < len(images) < 600:
-            print(os.getcwd())
-            print('before resampling: ', len(images))
-            for c in random.sample(os.listdir(), test_amount):
-                shutil.copy(c, os.getcwd())
-            print('before after resampling: ', len(images))
-        elif 1000 < len(images) < 1200:
-            print(os.getcwd())
-            print('before resampling: ', len(images))
-            pass
-        os.chdir(current_directory)
+#for dir in os.getcwd():
+#    longest_dir_length = max(len(dir))
+for folder in os.listdir():
+    current_directory = os.getcwd()
+    os.chdir(folder)
+    images = os.listdir()
+    if len(images) > 5000:
+        print(os.getcwd())
+        print('before resampling: ', len(images))
+#            for c in random.sample(os.listdir(), (len(images) / 2)):
+#                shutil.move(c, os.chdir('../../trash_images'))
+        print('before after resampling: ', len(images))
+    if len(images) < 150:
+        print(os.getcwd())
+        print('before resampling: ', len(images))
+#            for c in random.sample(os.listdir(), (len(images) * 5)):
+#                shutil.copy(c, os.getcwd())
+        print('before after resampling: ', len(images))
+    elif 300 < len(images) < 600:
+        print(os.getcwd())
+        print('before resampling: ', len(images) * 2)
+#            for c in random.sample(os.listdir(), test_amount):
+#                shutil.copy(c, os.getcwd())
+        print('before after resampling: ', len(images))
+    elif 1000 < len(images) < 1200:
+        print(os.getcwd())
+        print('before resampling: ', len(images))
+        pass
+    os.chdir(current_directory)
 current_dir = os.getcwd()
 os.chdir(current_dir)
