82b3086a89f08648f0a3b7fab19f92b588c0fab0
Leonor Drummond <84380184+ltadrummond@users.noreply.github.com>
2021-09-22 11:18:18 +0200

    add functions and metrics

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,4 +1,6 @@
 from functions import *
+#from ? import history
+
 
 # Calculating accuracy
 y_prediction = model.predict_classes(X_test, batch_size=32, verbose=0)
@@ -6,15 +8,15 @@ accuracy = accuracy_score(y_test, y_prediction)
 print('CNN accuracy: ',accuracy)
 
 # Compute confusion matrix
-cnf_matrix = confusion_matrix(y_test, y_pred_cnn)
+computed_confusion_metrix = confusion_matrix(y_test, y_pred_cnn)
 np.set_printoptions(precision=2)
 
 # Plot non-normalized confusion matrix
 plt.figure()
-plot_confusion_matrix(cnf_matrix, classes=['akiec','bcc','bkl','df','mel', 'nv', 'vasc'],
-                      title='Confusion matrix, without normalization')
+plot_confusion_matrix(computed_confusion_metrix, classes=['akiec','bcc','bkl','df','mel', 'nv', 'vasc'],
+                      title='Confusion matrix')
 plt.show()
 
-# extract the probability for the label that was predicted:
 
+# extract the probability for the label that was predicted:
 plot_probabilities(y_prediction)
